beee0e597a07d72296e3b11b8bb807f3ad793714
rahgawirajakumaraswamy <rahgawiraja1999@outlook.de>
2023-04-28 16:42:36 +0200

    added assets

--- src/pages/AnimalProfile.js
+++ src/pages/AnimalProfile.js
@@ -1,5 +1,36 @@
 import {useParams} from "react-router-dom";
-export default function AnimalProfile({items,assets}){
+export default function AnimalProfile({animals,animalAssets}){
     const {animalID} = useParams();
-   const singleAnimal= items.find((item)=>item.fields.animalId=== animalID);
+    
+   const singleAnimal= animals.find((item)=>item.fields.animalId=== animalID);
+    const singleAnimalAsset = animalAssets.find((item)=>item.sys.id===singleAnimal.fields.animalImage.sys.id);
+    const animalImageUrl ="https"+ singleAnimalAsset.fields.file.url;
+
+
+    return(
+     <div className ="animalProfile">
+        <div className= "animalImage">
+            <img src={animalImageUrl} alt={singleAnimal.Asset.fields.title} />
+        </div>
+        <div className= "description">
+            {singleAnimal.Asset.fields.description}
+        </div>
+        <div className="profile">
+            <ul>
+                <li>Breed:</li>
+                <li>Age:</li>
+                <li>Gender:</li>
+             </ul>
+        </div>
+    </div>
+
+
+
+    )
+
+
+
+
+
+
 }
