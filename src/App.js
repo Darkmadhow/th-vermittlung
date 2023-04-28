4c89a4a68e119d078dd105481f3dcc54fcbc9de9
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-04-28 16:16:38 +0200

    added Animals.js component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,48 @@
-import './App.css';
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import Home from "./pages/Home"
-import { Route, Routes } from 'react-router-dom';
-import Animals from './pages/Animals';
-import AnimalProfile from "./pages/AnimalProfile"
-import Contact from "./pages/Contact"
-import { useEffect, useState } from 'react';
+import "./App.css";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import { Route, Routes } from "react-router-dom";
+import Animals from "./pages/Animals";
+import AnimalProfile from "./pages/AnimalProfile";
+import Contact from "./pages/Contact";
+import { useEffect, useState } from "react";
 
 function App() {
-  const [animals, setAnimals] = useState();
+  const [animals, setAnimals] = useState([]);
 
-  useEffect(()=>{
+  useEffect(() => {
     getAnimals();
-    console.log(animals);
-  },[]);
+  }, []);
+  console.log("animals state", animals);
 
-  function getAnimals(){
-    let url = "https://cdn.contentful.com/spaces/3buz4oreveso/environments/master/entries?access_token=qRdu9vWWuio9t0XRmoN1e_cOEZDItF9_ePajyzX0o_E";
-    
-    fetch(url).then((response)=>{
-      if(!response.ok) throw new Error(`Couldn't load animals: ${response.status}`);
-      else return response.json();
-    }).then((data)=>{setAnimals(data)}).catch((error)=>{console.log(error)});
+  function getAnimals() {
+    let url =
+      "https://cdn.contentful.com/spaces/3buz4oreveso/environments/master/entries?access_token=qRdu9vWWuio9t0XRmoN1e_cOEZDItF9_ePajyzX0o_E";
+
+    fetch(url)
+      .then((response) => {
+        if (!response.ok)
+          throw new Error(`Couldn't load animals: ${response.status}`);
+        else return response.json();
+      })
+      .then((data) => {
+        setAnimals(data.items);
+        console.log("data", data);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
   }
 
   return (
     <div className="App">
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}/>
-        <Route path="animals" element={<Animals />}/>
-        <Route path="animalprofile/:animalID" element={<AnimalProfile />}/>
-        <Route path="contact" element={<Contact />}/>
+        <Route path="/" element={<Home />} />
+        <Route path="animals" element={<Animals />} />
+        <Route path="animalprofile/:animalID" element={<AnimalProfile />} />
+        <Route path="contact" element={<Contact />} />
       </Routes>
       <Footer />
     </div>
--- src/pages/Animals.js
+++ src/pages/Animals.js
@@ -1,3 +1,52 @@
-export default function Animals(){
-    
+import { useState } from "react";
+
+export default function Animals({ animals, animalAssets }) {
+  const [filteredAnimals, setFilteredAnimals] = useState(animals);
+
+  console.log("animals in animals", animals);
+  console.log("filteredAnimals", filteredAnimals);
+
+  function handleFilter(value) {
+    const results =
+      animals.length &&
+      animals.filter((animal) => {
+        return animal.fields.category === value;
+      });
+    console.log("results", results);
+    setFilteredAnimals(results);
+  }
+
+  return (
+    <div>
+      <div>
+        <button className="cat-btn" onClick={() => setFilteredAnimals(animals)}>
+          All
+        </button>
+        <button className="cat-btn" onClick={() => handleFilter("Dog")}>
+          Dog
+        </button>
+        <button className="cat-btn" onClick={() => handleFilter("Cat")}>
+          Cat
+        </button>
+        <button className="cat-btn" onClick={() => handleFilter("Rodent")}>
+          Rodent
+        </button>
+        <button className="cat-btn" onClick={() => handleFilter("Bird")}>
+          Bird
+        </button>
+        {filteredAnimals.length
+          ? filteredAnimals.map((item) => {
+              return (
+                <li className="animal-card">
+                  <img src={item.fields.animalImage.sys} alt="name" />
+                  <h3>{item.fields.name}</h3>
+                  <p> {item.fields.age} </p>
+                  <p> {item.fields.gender} </p>
+                </li>
+              );
+            })
+          : " ...loading"}
+      </div>
+    </div>
+  );
 }
