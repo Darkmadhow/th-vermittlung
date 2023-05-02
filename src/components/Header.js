5d6cd6665caaf78ab3607f742a69ba03855a4301
MariaA-109 <122259443+MariaA-109@users.noreply.github.com>
2023-05-02 17:49:34 +0200

    added css, Header & Footer

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
 import { NavLink } from "react-router-dom";
+import "./Header.css";
+import backgroundImage from "../img/header.jpg";
 
 export default function Header() {
   return (
-    <header>
+    <header
+      style={{
+        backgroundImage: `url(${backgroundImage})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >
+      <div className="h1-background">
+        <h1>Tiervermittlung e.V.</h1>
+      </div>
       <nav>
-        <NavLink to="/">Home</NavLink>
-        <NavLink to="animals">See our Animals</NavLink>
-        <NavLink to="contact">Contact</NavLink>
+        <NavLink className="menubtns" to="/">
+          Home
+        </NavLink>
+        <NavLink className="menubtns" to="animals">
+          See our Animals
+        </NavLink>
+        <NavLink className="menubtns" to="contact">
+          Contact
+        </NavLink>
       </nav>
     </header>
   );
