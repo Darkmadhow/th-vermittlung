5d8e711e5fd26ca2f8ef96dbeffbf4c1c6549715
rahgawirajakumaraswamy <rahgawiraja1999@outlook.de>
2023-05-08 16:37:05 +0200

    changed title of interest -form

--- src/components/InterestForm.js
+++ src/components/InterestForm.js
@@ -2,7 +2,7 @@ import "./InterestForm.css";
 export default function InterestForm({singleAnimal}){
     return (
         <div className="interest-form">
-            <h3>Interest Form</h3>
+            <h3>You are interested in adopting a pet?</h3>
         <form action="#">
           <div className="input-name">
           <label htmlFor="name">Name:</label>
